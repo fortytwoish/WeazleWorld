dd0360ec552e139b31079143e0bfc7620d42aac8
Marvin Müller <marvinmueller_92@hotmail.de>
2016-05-26 19:47:11 +0000

    Initial Commit cleanup 3 Imported from Changeset 15

--- Grundschulprojekt/js/MainScript.js
+++ Grundschulprojekt/js/MainScript.js
@@ -1,32 +1,72 @@
-﻿var camera, scene, renderer;
+﻿//Global variables
+isInMenu = true;
+
+//Local variables;
+var camera, scene, renderer;
 var geometry, material, mesh;
+var rot = 0;
 
 function init() {
-    alert("test");
-    renderer = new THREE.CanvasRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(880, 540);
     document.body.appendChild(renderer.domElement);
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10);
-    camera.position.z = 0.25;
+    camera.position.z = 0.75;
 
     scene = new THREE.Scene();
 
-    geometry = new THREE.PlaneGeometry(1, 1, 50, 50);
-    material = new THREE.MeshBasicMaterial({ color: 0x44DD00, wireframe: true });
+    geometry = new THREE.PlaneGeometry(1, 1, 16, 16);
+    material = new THREE.MeshBasicMaterial({ color: 0x00FF00, wireframe: true });
 
     mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
 
+    scene.add(mesh);
 }
 
 function animate() {
+    if (!isInMenu) {
+        mesh.rotation.z = (rot += 0.005);
+    }
 
     requestAnimationFrame(animate);
-
     renderer.render(scene, camera);
-
 }
+
+$(function () {
+
+    $("#menuButton").click(function () {
+        if (isInMenu)
+            return;
+        isInMenu = true;
+        console.log("menu button clicked.");
+        $("#menu").css("visibility", "visible");
+        printMenuState("css");
+        $("#menuButton").css("visibility", "hidden");
+    });
+
+    $("#minigameButton").click(function () {
+        if (isInMenu)
+            return;
+        isInMenu = true;
+        minigameID = prompt("Which minigame? (1-2)");
+        console.log("minigame button clicked.");
+        $("#minigame").css("visibility", "visible");
+        printMenuState("css");
+        console.log("Reading: " + "html/minigame" + minigameID + ".html");
+        $.get("html/minigame" + minigameID + ".html", function (data) {
+            console.log("Contents: " + data);
+            $("#minigame").html(data);
+        });
+
+    });
+});
 
 init();
 animate();
+
+function printMenuState(s) {
+    menu = $("#menu");
+    console.log("menu: " + menu + " dimensions: " + menu.css("left") + ", " + menu.css("top") + ", " + menu.width() + " X " + menu.height() + " visibility: " + menu.css("visibility") + " " + s);
+}
